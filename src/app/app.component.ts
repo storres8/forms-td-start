9ebb0198d5ec05a3d52a122bc1b9fb6efd834629
storres8 <stevetorres7562@gmail.com>
2019-07-14 19:50:38 -0400

    got the values of the form throught the @ViewChild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -7,12 +7,20 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild("f", { static: false }) signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = "Superuser";
   }
 
-  // the form data that comes in is actually a JS object but is of type ngForm by default bc of angular.
-  onSubmit(f: NgForm) {
-    console.log(f);
+  // // the form data that comes in is actually a JS object but is of type ngForm by default bc of angular.
+  // onSubmit(f: NgForm) {
+  //   console.log(f);
+  // }
+
+  // Since we are using a reference in the template for our form we can also access
+  // our from with the @ViewChild.
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
